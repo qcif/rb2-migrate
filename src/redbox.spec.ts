8bd58b6c037f3639185a0689401af7e4e7ef86fe
Mike Lynch <mike@mikelynch.org>
2018-05-17 10:55:02 +1000

    Fetch metadata works

--- src/redbox.spec.ts
+++ src/redbox.spec.ts
@@ -13,19 +13,33 @@ const config = require('config');
 const TEST19PTS = [ 'dmpt', 'dataset', 'self-submission' ];
 
 
+function rbconnect(server: string):Redbox {
+    const baseURL = config.get('servers.' + server + '.url');
+    const apiKey = config.get('servers.' + server + '.apiKey');
+    return new Redbox(baseURL, apiKey);
+}
 
-describe('Redboches', function() {
+describe('Redbox', function() {
 
     it('can fetch lists of objects from 1.9', async () => {
 	this.timeout(10000);
-	const baseURL = config.get('servers.Test1_9.url');
-	const apiKey = config.get('servers.Test1_9.apiKey');
-	const rb = new Redbox(baseURL, apiKey);
+	const rb = rbconnect('Test1_9');
 	for( var i in TEST19PTS ) {
 	    let pt = TEST19PTS[i];
 	    const oids = await rb.search(pt);
             expect(oids).to.not.be.empty;
 	}
+    });
+
+    it('can fetch a metadata object from 1.9', async () => {
+	this.timeout(10000);
+	const rb = rbconnect('Test1_9');
+	const oids = await rb.search('dmpt');
+        expect(oids).to.not.be.empty;
+	const oid = oids[0];
+	const md = await rb.recordmeta(oid);
+	expect(md).to.not.be.null;
+	expect(md['oid']).to.equal(oid);
     });
 
 });
