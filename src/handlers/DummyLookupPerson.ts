d75a52617fbe70782dd47d86db2397f8790c62bf
Shilo B <shiloworks@gmail.com>
2018-11-06 11:24:48 +1000

    Added more name fields for dummys.

--- src/handlers/DummyLookupPerson.ts
+++ src/handlers/DummyLookupPerson.ts
@@ -17,9 +17,10 @@ export class DummyLookupPerson extends LookupPerson  {
   insertDummys(o, mainObj) {
     if (_.isEmpty(o['given_name']) || _.isUndefined(o['given_name'])) {
         o['given_name'] = "John";
-    }
-    if (_.isEmpty(o['family_name']) || _.isUndefined(o['family_name'])) {
         o['family_name'] = "Doe";
+        o["text_full_name"] = `${o['given_name']} ${o['family_name']}`
+        o["full_name_honorific"] = `${o['given_name']} ${o['family_name']}`
+        o["full_name_family_name_first"] = `${o['family_name']}, ${o['given_name']}`
     }
     if (_.isEmpty(o['email']) || _.isUndefined(o['email'])) {
         o['email'] = `${o['given_name']}.${o['family_name']}@redboxresearchdata.com.au`;
