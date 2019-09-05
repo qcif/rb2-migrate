975a59f3d78a6c84da1ab13779238f8f895f7b79
mattRedBox <matt@redboxresearchdata.com.au>
2019-09-05 10:48:35 +1000

    updated grants and funding bodies. ensured changes in dataset live copied to other stages.

--- src/handlers/FundingBody.ts
+++ src/handlers/FundingBody.ts
@@ -1,20 +1,29 @@
 // FundingBody handler
 
-import { Handler, HandlerBase } from './handlers';
-
+import {Handler, HandlerBase} from './handlers';
+import * as _ from 'lodash';
 
 export class FundingBody extends HandlerBase implements Handler {
 
-  crosswalk(o:Object, mainObj?:any): Object|undefined {
-
-    return {
-    	'dc_title': o['dc_title'],
-    	'dc_identifier': [ o ['dc_identifier'] ],
-    	'ID': o['dc_title'],
-    	'repository_name': [
-    		'Funding Bodies'
-    	]
-    };
+  crosswalk(o: Object): Object | undefined {
+    let title = o['dc_title'];
+    let repository_name = 'Funding Bodies';
+    const matches = title.match(/^\((.*)\)[\s]*(.+)$/);
+    if (matches && matches[1]) {
+      repository_name = matches[1];
+      if (matches[2]) {
+        title = matches[2];
+      }
+    }
+    if (!_.isEmpty(title)) {
+      return {
+        'dc_title': o['dc_title'],
+        'dc_identifier': [o ['dc_identifier']],
+        'repository_name': [
+          'Funding Bodies'
+        ]
+      };
+    }
   }
 
 }
--- src/handlers/GrantBody.ts
+++ src/handlers/GrantBody.ts
@@ -1,20 +1,30 @@
 // GrantBody handler
 
 import {Handler, HandlerBase} from './handlers';
-
+import * as _ from 'lodash';
 
 export class GrantBody extends HandlerBase implements Handler {
 
-	crosswalk(o: Object): Object | undefined {
-
-		return {
-			'dc_title': o['dc_title'],
-			'dc_identifier': [o ['dc_identifier']],
-			'grant_number': o['grant_number'],
-			'repository_name': [
-				'Research Activities'
-			]
-		};
-	}
+  crosswalk(o: Object): Object | undefined {
+    let title = o['dc_title'];
+    let repository_name = 'Research Activities';
+    const matches = title.match(/^\((.*)\)[\s]*(.+)$/);
+    if (matches && matches[1]) {
+      repository_name = matches[1];
+      if (matches[2]) {
+        title = matches[2];
+      }
+    }
+    if (!_.isEmpty(title)) {
+      return {
+        'dc_title': title,
+        'dc_identifier': [o ['dc_identifier']],
+        'grant_number': [o['grant_number']],
+        'repository_name': [
+          repository_name
+        ]
+      };
+    }
+  }
 
 }
